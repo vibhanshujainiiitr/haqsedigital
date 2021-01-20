00072babd941e28bad5808bdac796f315c48fe43
vibhanshujainiiitr <cs19b1027@iiitr.ac.in>
2021-01-20 18:10:56 +0530

    Some small chnages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -115,9 +115,9 @@ $(window).scroll(function () {
 	}
 });
 
-// $('.back-to-top').click(function () {
-// 	$('html, body').animate({
-// 		scrollTop: 0
-// 	}, 1500, 'easeInOutExpo');
-// 	return false;
-// });
+$('.back-to-top').click(function () {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 1500, 'easeInOutExpo');
+	return false;
+});
